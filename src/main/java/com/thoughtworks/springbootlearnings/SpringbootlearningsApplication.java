50428dfb45b51268e23d3b713c1e547e7c67f431
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-04-11 14:50:14 +0530

    Initialized the project setup

---
+++ src/main/java/com/thoughtworks/springbootlearnings/SpringbootlearningsApplication.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.springbootlearnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootlearningsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootlearningsApplication.class, args);
+		System.out.println("This is for testing purpose");
+	}
+
+}
