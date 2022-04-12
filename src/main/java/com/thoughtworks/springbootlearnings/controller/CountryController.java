972cbaba4b6006380cc4334b3279adc8db0ff743
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-04-12 11:13:45 +0530

    To get the list of countries using get mapping

--- src/main/java/com/thoughtworks/springbootlearnings/controller/CountryController.java
+++ src/main/java/com/thoughtworks/springbootlearnings/controller/CountryController.java
@@ -1,2 +1,33 @@
-package com.thoughtworks.springbootlearnings.controller;public class CountryController {
+package com.thoughtworks.springbootlearnings.controller;
+
+import com.thoughtworks.springbootlearnings.entity.Country;
+import com.thoughtworks.springbootlearnings.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    public CountryService countryService;
+
+    @GetMapping("/getcountries")
+    public List getCountries(){
+        return countryService.getAllCountries();
+    }
+
+    @GetMapping("/getcountries/{id}")
+    public Country getCountriesById(@PathVariable int id){
+        return countryService.getCountryById(id);
+    }
+
+    @GetMapping("/getcountries/countryName")
+    public Country getCountriesByName(@RequestParam (value = "name") String countryName){
+        return countryService.getCountryByName(countryName);
+    }
 }
--- src/main/java/com/thoughtworks/springbootlearnings/service/CountryService.java
+++ src/main/java/com/thoughtworks/springbootlearnings/service/CountryService.java
@@ -1,2 +1,46 @@
-package com.thoughtworks.springbootlearnings.service;public class CountryService {
+package com.thoughtworks.springbootlearnings.service;
+
+import com.thoughtworks.springbootlearnings.entity.Country;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Component
+public class CountryService {
+
+    HashMap<Integer, Country> countryMap;
+
+    public CountryService() {
+
+        countryMap = new HashMap<Integer, Country>();
+
+        Country indiaCountry = new Country(1, "India", "Delhi");
+        Country ukCountry = new Country(2, "UK", "London");
+
+        countryMap.put(1, indiaCountry);
+        countryMap.put(2, ukCountry);
+    }
+
+    public List getAllCountries() {
+        List countries = new ArrayList(countryMap.values());
+        return countries;
+    }
+
+    public Country getCountryById(int id) {
+        Country country = countryMap.get(id);
+        return country;
+    }
+
+    public Country getCountryByName(String countryName) {
+        Country country = null;
+        for (int i : countryMap.keySet()) {
+            if (countryMap.get(i).getCountryName().equals(countryName))
+                country = countryMap.get(i);
+        }
+        return country;
+    }
+
+
 }
